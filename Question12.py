838d478c31881c6704020a3767a7c8fade711f8f
My name <mandal9099@gmail.com>
2024-04-23 23:44:11 +0530

    update

---
+++ Question12.py
@@ -0,0 +1,4 @@
+#Write a program to find max of 2 numbers
+num1 = int(input("Enter your number1:- "))
+num2 = int(input("Enter your number2:- "))
+print(f"maximum number is {num1}")if num1>num2 else print(f"maximum number is {num2}")
