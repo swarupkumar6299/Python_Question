60203ccb2c6802bdd27511a0131a834f9e56a817
My name <mandal9099@gmail.com>
2024-04-24 00:01:05 +0530

    update

--- Question10.py
+++ Question10.py
@@ -1,2 +1,9 @@
 #Write a program to convert input alphabet from uppercase to lowercase
 #and lowercase to uppercase
+character1 = (input("Enter your upper character:- "))
+character2 = (input("Enter your lower character:- "))
+lower_case = character1.lower()
+upper_case = character2.upper()
+
+print(f"your character is lower case is {lower_case}")
+print(f"your character is upper case is {upper_case}")
