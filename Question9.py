ec359a12bb52d25beaa5508500382e3e48cc96c3
My name <mandal9099@gmail.com>
2024-04-23 23:09:17 +0530

    add

---
+++ Question9.py
@@ -0,0 +1,3 @@
+#Write a program to find input number is +ve,-ve or zero
+num = int(input("Enter the number:- "))
+print(f"Your number {num} is possitive")if num>=0 else print(f"Your number {num} is negative")
