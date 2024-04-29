ff11509aedb02b053f066152f990627a21909bb3
My name <mandal9099@gmail.com>
2024-04-29 19:54:19 +0530

    update

---
+++ Question.py
@@ -0,0 +1,5 @@
+#write a program to print number from 1 to 10 using while loop
+num = 1 #--------> insilize
+while num<=10: #-------> condition statement
+    print(num,end=' ')
+    num = num+1 #------->update statement
