33b44e92107dc25d3dae6300c18db723a2d8691a
My name <mandal9099@gmail.com>
2024-05-02 23:40:43 +0530

    update

--- Question46.py
+++ Question46.py
@@ -82,6 +82,13 @@ for r in range(5,0,-1):
         print(c, end=' ')
     print()
 print("----------------------------------")
+"""
+1
+1 2
+1 2 3
+1 2 3 4
+1 2 3 4 5
+"""
 for r in range(1,6):
     for c in range(1,r+1):
         print(r,end=' ')
